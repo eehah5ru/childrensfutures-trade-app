cd054a8240f4f9c4c2105b2b1f30b8123febfb59
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-01-29 18:37:56 +0300

    [app] myfutures promo video overlay

--- src/cljs/childrensfutures_trade/components/new_goal.cljs
+++ src/cljs/childrensfutures_trade/components/new_goal.cljs
@@ -11,7 +11,7 @@
     ))
 
 
-(defn- new-goal-view [new-goal]
+#_(defn- new-goal-view [new-goal]
   [:div
    [:h1 "New Goal"]
    [ui/text-field
@@ -36,14 +36,22 @@
      :style {:width "100%"}}]
    ])
 
+(defn- new-goal-view []
+  [:iframe
+   {:src "https://www.youtube.com/embed/2rzdt3ytH_Y?rel=0&autoplay=1"
+    :frame-border "0"
+    :allow-full-screen true
+    :style {:position "absolute"
+            :top 0
+            :left 0
+            :width "100%"
+            :height "100%"}}])
 
 (defn new-goal-dialog []
   (let [show-new-goal? (subscribe [:ui/show-new-goal?])
-        new-goal (subscribe [:db/new-goal])
         create-button [ui/raised-button
                        {:secondary true
-                        :disabled (or (empty? (:description @new-goal))
-                                      (:trx-on-air? @new-goal))
+                        :disabled false
                         :label "Place on Exchange"
                         :style {:margin-top 15}
                         :on-touch-tap #(dispatch [:blockchain.new-goal/send])}]
@@ -54,11 +62,15 @@
                         :on-touch-tap #(dispatch [:ui.new-goal/toggle-view])}]]
 
     [ui/dialog
-     {:modal true
-      :actions [(r/as-element create-button)
-                (r/as-element cancel-button)]
-      :children (r/as-element [new-goal-view @new-goal])
-      :open @show-new-goal?}]))
+     {:modal false
+      :body-style {:position "relative"
+                   :padding-bottom "56.25%" ; /* 16:9 */
+                   :padding-top "25px"      ;
+                   :height "0"}
+      :actions []
+      :on-request-close #(dispatch [:ui.new-goal/toggle-view])
+      :open @show-new-goal?}
+     [new-goal-view]]))
 
 ;;;
 ;;; NEW GOAL BUTTON
