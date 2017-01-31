4f0145a3a8ba29f4182bbc68bc98c37467e6d0ba
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-02-01 00:15:57 +0300

    [contract] implementation of Chat

--- truffle/contracts/Chat.sol
+++ truffle/contracts/Chat.sol
@@ -3,5 +3,31 @@ pragma solidity ^0.4.2;
 import "./owned.sol";
 
 contract Chat is owned {
-  event MessageAdded(bytes32 channelId, address sender, string message);
+  uint msgId;
+
+  event MessageSent(bytes32 channelId, uint msgId, address sender, string message);
+
+
+  //
+  // setup
+  //
+  function Chat()
+    public
+    payable
+  {
+    msgId = 0;
+  }
+
+  //
+  // send message
+  //
+  function sendMessage(bytes32 _channelId, string _message)
+    public
+    returns(bool)
+  {
+    msgId++;
+
+    MessageSent(_channelId, msgId, msg.sender, _message);
+    return true;
+  }
 }
