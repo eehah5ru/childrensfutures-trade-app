9bf28d541b598e7d8e5897460f8419f8b6b35fd8
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-02-01 21:54:35 +0300

    [contract] added Chat contract into compile solidity script

--- compile-solidity.sh
+++ compile-solidity.sh
@@ -3,4 +3,11 @@ CONTRACTS_PATH="$(pwd)/resources/public/contracts/"
 SRC_PATH="$CONTRACTS_PATH/src"
 BUILD_PATH="$CONTRACTS_PATH/build"
 cd $SRC_PATH
+#
+# GSE
+#
 solc --optimize --bin --abi --combined-json bin,abi GoalsStockExchange.sol -o $BUILD_PATH > $BUILD_PATH/GoalsStockExchange.json
+#
+# Chat
+#
+solc --optimize --bin --abi --combined-json bin,abi Chat.sol -o $BUILD_PATH > $BUILD_PATH/Chat.json
