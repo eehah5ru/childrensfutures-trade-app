1e3653aec4437d076e79d22673450b7c715bb4db
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-05-04 13:34:37 +0300

    [deploy][production] added creating of index.html to deploy script

--- deploy_to_myfutures_trade.sh
+++ deploy_to_myfutures_trade.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "deploying to myfutures.trade" &&
+    lein clean &&
+    lein with-profile uberjar filegen-ng &&
     LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar &&
     scp ./target/childrensfutures-trade.jar do.myfutures.trade:/var/myfutures-trade &&
     ssh do.myfutures.trade "sudo service myfutures-trade restart; sleep 10; sudo service myfutures-trade status"
