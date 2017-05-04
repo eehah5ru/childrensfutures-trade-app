de02ecf80f8629dd6be6382e2c4951a37734fb3a
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-05-04 13:47:18 +0300

    [deploy][production] removed force clean from deploy process

--- deploy_to_myfutures_trade.sh
+++ deploy_to_myfutures_trade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deploying to myfutures.trade" &&
-    lein clean &&
+    # lein clean &&
     lein with-profile uberjar filegen-ng &&
     LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar &&
     scp ./target/childrensfutures-trade.jar do.myfutures.trade:/var/myfutures-trade &&
