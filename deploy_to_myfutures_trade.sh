ef99288323374f4fc7386fad954b9e70f0b9ca1a
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-05-04 12:52:23 +0300

    [deploy]  LEIN_SNAPSHOTS_IN_RELEASE=true in deploy script to mf.t

--- deploy_to_myfutures_trade.sh
+++ deploy_to_myfutures_trade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "deploying to myfutures.trade" &&
-    lein uberjar &&
+    LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar &&
     scp ./target/childrensfutures-trade.jar do.myfutures.trade:/var/myfutures-trade &&
     ssh do.myfutures.trade "sudo service myfutures-trade restart; sleep 10; sudo service myfutures-trade status"
