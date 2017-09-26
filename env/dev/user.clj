3b86911256adf7ffa69e55f031c57cdefcb6820e
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-09-26 15:12:04 +0200

    typo

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,7 +1,7 @@
- (ns user
-   (:require [figwheel-sidecar.repl-api]
-             [childrensfutures-trade.core]
-             [ring.middleware.reload :refer [wrap-reload]]))
+(ns user
+  (:require [figwheel-sidecar.repl-api]
+            [childrensfutures-trade.core]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
